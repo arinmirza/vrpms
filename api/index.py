6d70d5da1764c1a67272b68554024913d840cf4f
arinmirza <arinmirza@gmail.com>
2023-09-20 21:07:42 +0200

    Test

--- api/index.py
+++ api/index.py
@@ -1,10 +1,7 @@
 import json
 from http.server import BaseHTTPRequestHandler
 
-#import src.solver
-#import src.utilities.today
-
-import os
+# Absolute import modules inside src folder
 import src.utilities.today
 import src.utilities.foo
 import src.solver
@@ -17,22 +14,12 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # Construct an example response 
-        #body = {
-        #    'vrp_solution': src.solver.solve_vrp_problem(),
-        #    'travel_duration': src.solver.calculate_duration("Unity Beta", "Marienplatz"),
-        #    'current_date': src.utilities.today.get_current_date(),
-        #}
-
         body = {
-            #'today': src.utilities.today.get_current_date(),
-            'solver': src.solver.solve_vrp_problem(),
-            #'foo': src.utilities.foo.print_info(),
-            #'cwd': os.getcwd(),
-            #'cwd_content': os.listdir(os.getcwd()),
-            #'parent': os.path.dirname(os.getcwd()),
-            #'parent_content': os.listdir(os.path.dirname(os.getcwd())),
+            'vrp_solution': src.solver.solve_vrp_problem(),
+            'travel_duration': src.solver.calculate_duration("Unity Beta", "Marienplatz"),
+            'current_date': src.utilities.today.get_current_date(),
         }
-
+        
         # Convert the dictionary into JSON and serialize it, then encode as utf8
         encoded_body = json.dumps(body).encode('utf-8')
 
