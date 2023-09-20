4dd04ff7ceb98b69b04da33491a783f9db4e6a2e
arinmirza <arinmirza@gmail.com>
2023-09-20 20:05:49 +0200

    Fix folder structure and imports

--- api/index.py
+++ api/index.py
@@ -1,11 +1,8 @@
 import json
 from http.server import BaseHTTPRequestHandler
 
-#from src.example_folder import bar
-#from src.foo import solve_vrp_problem
-#from src.foo import calculate_duration
-from src.example_folder import bar
-
+import src.solver
+import src.utilities.today
 
 class handler(BaseHTTPRequestHandler):
 
@@ -15,13 +12,11 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # Construct an example response 
-        #body = {
-        #    'vrp_solution': solve_vrp_problem(),
-        #    'travel_duration': calculate_duration("Unity Beta", "Marienplatz"),
-        #    'current_date': bar.get_current_date(),
-        #}
-
-        body = { 'Hello': 'World!'}
+        body = {
+            'vrp_solution': src.solver.solve_vrp_problem(),
+            'travel_duration': src.solver.calculate_duration("Unity Beta", "Marienplatz"),
+            'current_date': src.utilities.today.get_current_date(),
+        }
 
         # Convert the dictionary into JSON and serialize it, then encode as utf8
         encoded_body = json.dumps(body).encode('utf-8')
