ad25ada9fc6c1413a4ad1ae2dc28fff939085a01
arinmirza <arinmirza@gmail.com>
2023-09-20 19:30:13 +0200

    bugfix

--- api/index.py
+++ api/index.py
@@ -1,9 +1,9 @@
 import json
 from http.server import BaseHTTPRequestHandler
 
-from src.example_folder import bar
-from src.foo import solve_vrp_problem
-from src.foo import calculate_duration
+#from src.example_folder import bar
+#from src.foo import solve_vrp_problem
+#from src.foo import calculate_duration
 
 
 class handler(BaseHTTPRequestHandler):
@@ -14,11 +14,13 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # Construct an example response 
-        body = {
-            'vrp_solution': solve_vrp_problem(),
-            'travel_duration': calculate_duration("Unity Beta", "Marienplatz"),
-            'current_date': bar.get_current_date(),
-        }
+        #body = {
+        #    'vrp_solution': solve_vrp_problem(),
+        #    'travel_duration': calculate_duration("Unity Beta", "Marienplatz"),
+        #    'current_date': bar.get_current_date(),
+        #}
+
+        body = { 'Hello': 'World!'}
 
         # Convert the dictionary into JSON and serialize it, then encode as utf8
         encoded_body = json.dumps(body).encode('utf-8')
