9ba901d916ff5f237809eaa68eefff226cb96e3a
yusuffserdar <yusufserdar1700@gmail.com>
2023-10-22 21:55:27 +0200

    perf improv single core run fastest

--- api/index.py
+++ api/index.py
@@ -25,8 +25,8 @@ class handler(BaseHTTPRequestHandler):
             "current_date": src.utilities.helper.get_current_date(),
             #"metes_solution": run(),
             #"yusuf_solution_genetic_algorithm_all_cores": genetic_algorithm_mc_fast(multithreaded=True),
-            "yusuf_solution_genetic_algorithm_single_core_fast": genetic_algorithm_sc_fast(multithreaded=False),
-            #"yusuf_solution_genetic_algorithm_single_core_fastest": genetic_algorithm_sc_fastest(multithreaded=False)
+            #"yusuf_solution_genetic_algorithm_single_core_fast": genetic_algorithm_sc_fast(multithreaded=False),
+            "yusuf_solution_genetic_algorithm_single_core_fastest": genetic_algorithm_sc_fastest(multithreaded=False)
 
         }
 
