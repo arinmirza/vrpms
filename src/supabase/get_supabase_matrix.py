32e98c5a6ac8f16b945beac0b8cada73fb4f5645
yusuffserdar <yusufserdar1700@gmail.com>
2023-11-01 01:11:22 +0100

    supabase matrix reading update

--- src/supabase/get_supabase_matrix.py
+++ src/supabase/get_supabase_matrix.py
@@ -1,10 +1,16 @@
 import numpy as np
 #import os
 #CURRENT_PATH = os.getcwd()
+
+import os
+
+cur_path = os.path.dirname(__file__)
+
+
 def get_data():
 
     # this method will be called from control.py, thus "/supabase" folder is added to the CURRENT_PATH
-    duration_matrix = np.load(file ="dataset_matrix.npy", allow_pickle=True)
+    duration_matrix = np.load(file =str(cur_path)+"/dataset_matrix.npy", allow_pickle=True)
 
     return list (duration_matrix)
 
