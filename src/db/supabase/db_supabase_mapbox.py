a9e0ee14946449a07138323a9503451ee65a687c
Metehan Kaya <metehkaya96@gmail.com>
2023-11-18 23:10:38 +0100

    Supabase load data row fetch

--- src/db/supabase/db_supabase_mapbox.py
+++ src/db/supabase/db_supabase_mapbox.py
@@ -1,6 +1,8 @@
 from typing import List, Optional, Tuple
 from supabase import create_client, Client
 
+DEPOT = 0
+
 
 def get_url_key(file_path: str) -> Tuple[str, str]:
     file = open(file_path, "r")
@@ -34,9 +36,33 @@ def get_mapbox_duration_data(
     supabase_client = get_supabase_client(url, key, supabase_url_key_file)
     query = supabase_client.table(table_name).select("*").eq(column=query_column_name, value=query_row_id).execute()
     mapbox_data = query.data[0][data_column_name]
-    print(mapbox_data)
+    # print(mapbox_data)
     return mapbox_data
+
+
+def get_mapbox_load_data(
+    url: Optional[str] = None,
+    key: Optional[str] = None,
+    supabase_url_key_file: Optional[str] = "../../../data/supabase/supabase_url_key.txt",
+    query_row_id: int = 1,
+    n: int = 25,
+    table_name: str = "locations",
+    query_column_name: str = "id",
+    data_column_name: str = "json",
+):
+    supabase_client = get_supabase_client(url, key, supabase_url_key_file)
+    query = supabase_client.table(table_name).select("*").eq(column=query_column_name, value=query_row_id).execute()
+    mapbox_data = query.data[0][data_column_name]
+    # print(mapbox_data)
+    load = [0 for _ in range(n)]
+    for customer in mapbox_data:
+        id, demand = customer["id"], customer["demand"]
+        if DEPOT < id < n:
+            load[id] = demand
+    # print(load)
+    return load
 
 
 if __name__ == "__main__":
     get_mapbox_duration_data()
+    get_mapbox_load_data()
