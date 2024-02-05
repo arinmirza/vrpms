a53aece2c4ecde3bcdeadadaf1b789d57f38a92c
arinmirza <arinmirza@gmail.com>
2024-02-06 00:14:41 +0100

    Remove credentials

--- src/get_token.py
+++ src/get_token.py
@@ -3,8 +3,8 @@ from supabase.client import create_client, Client
 # Set login credentials
 URL: str = "https://pkeygmzuwfucblldmkjn.supabase.co"
 KEY: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrZXlnbXp1d2Z1Y2JsbGRta2puIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc5NzU0MTksImV4cCI6MjAxMzU1MTQxOX0.5TsK_pH0xsMyJWo_XPXt4NhsuS-vW6MAcj575WskZ8s"
-EMAIL: str = "yusufserdar1700@gmail.com"
-PASSWORD: str = "yusufgym123!"
+EMAIL: str = ""
+PASSWORD: str = ""
 
 # Connect and login
 supabase: Client = create_client(URL, KEY)
